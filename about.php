e501c8a9393db40ad353519141793b40c4367229
Allan <owinoallan77@gmail.com>
2023-03-27 18:43:19 +0300

    web.php

---
+++ about.php
@@ -0,0 +1,23 @@
+<?php
+    $con = mysqli_connect('localhost', 'root');
+
+    if($con) {
+      echo 'Connection Successful';
+    }
+    else{
+      echo 'Connection Failed';
+    }
+
+    mysqli_select_db($con, 'table1');
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $number = $_POST['number'];
+
+
+    $query = "INSERT INTO users (name, email, number) VALUES ('$name', '$email', '$number')";
+
+mysqli_query($con, $query);
+header('location:index.php#contact');
+
+?>
---
+++ index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>@Allashe.php</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+<body>
+  
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Allashe Photos</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#home">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Cartegories
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#school">School</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#work">Work</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#travel">Travel</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about">About</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="images/pexels-photo-534169.jpeg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/pexels-photo-533975.jpeg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/pexels-photo-534174.jpeg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<a id="school">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">School</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/sun-flower-blossom-bloom-pollen-541484.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-533975.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534220.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  </div>
+  </div>
+
+</section>
+</a>
+
+<a id="work">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Work</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534124.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534174.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534283.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  </div>
+  </div>
+</section>
+</a>
+
+<a  id="travel">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Travel</h2>
+  </div>
+  <div class="container-fluid">
+    <div class="row">
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534210.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-534124.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  <div class="col-lg-4 col-md-4 col-12">
+    <img src="images/pexels-photo-533982.jpeg" alt="sunflower" class="img-fluif pb-3">
+  </div>
+  </div>
+  </div>
+</section>
+</a>
+
+<a id="contact">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">Contact Us</h2>
+  </div>
+
+  <div class="w-50 m-auto">
+    <form action="about.php" method="post">
+      <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" name="email" class="form-control">
+      </div>
+      <div class="form-group">
+        <label for="number">Number</label>
+        <input type="text" name="number" class="form-control">
+      </div>
+      <button type="submit" class="btn btn-success">Submit</button>
+    </form>
+  </div>
+</section>
+</a>
+
+<a  id="about">
+<section class="my-4">
+  <div class="py-4">
+    <h2 class="text-center">About Us</h2>
+  </div>
+  <div class="container-fluid">
+    <h3 class="text-center">Allan</h3><br>
+    <p class="text-center"><b>I am a passionate photographer interested in working in Architecture Nature and Travel photography.
+      I am a full time freelancer with an experience of two years
+    </b></p>
+  </div>
+</section>
+</a>
+
+
+<script scr="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script scr="https://cdnjs.cloudfare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script scr="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+</body>
+</html>
